f9c40e6a1309fc1455120319c926a8b2e84ed491
Sébastien Mandaba <seb.mandaba@gmail.com>
2019-05-16 00:12:51 +0200

    updated urls

--- food2/food2/urls.py
+++ food2/food2/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("app.urls")),
+    path('food2/admin/', admin.site.urls),
+    path('food2', include("app.urls")),
     url(r'^i18n/', include('django.conf.urls.i18n')),
 ]
